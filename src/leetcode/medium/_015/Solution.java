9e27742779132bd6fb11988eb6c80887ea73498c
Weikoi <huangzengrui@yahoo.com>
2019-05-02 15:52:57 +0800

    leetcode Java 15

--- src/leetcode/medium/_015/Solution.java
+++ src/leetcode/medium/_015/Solution.java
@@ -1,25 +1,4 @@
 package leetcode.medium._015;
-<<<<<<< HEAD
-import java.util.*;
-
-public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int two_sum = 0 - nums[i];
-            int l =0;
-            int r = nums.length-1;
-            while(l<r){
-                if(nums[l] + nums[r] == two_sum){
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    while(l<r && nums[l] == nums[l+1]) l++;
-                    while(l<r && nums[r] == nums[r-1]) r--;
-                    l++;
-                    r--;
-                }else if(nums[l] + nums[r] < two_sum) l++;
-=======
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -44,7 +23,6 @@ public class Solution {
                     l++;
                     r--;
                 }else if(nums[l]+nums[r]<two_sum) l++;
->>>>>>> origin/master
                 else r--;
             }
         }
@@ -54,4 +32,5 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(new Solution().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
+
 }
