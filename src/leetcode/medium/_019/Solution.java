f5875030aa0bb33cc3240e388dde115c4f65987b
huangxiaowei <huangzengrui126@126.com>
2019-05-03 14:25:33 +0800

    014

---
+++ src/leetcode/medium/_019/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.medium._019;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        if(head.next==null || head==null) return null;
+        ListNode fir =head;
+        ListNode sec =head;
+        while(n!=0){
+            fir = fir.next;
+            n--;
+        }
+        if(fir.next!=null){
+            while(fir.next!=null){
+                fir=fir.next;
+                sec=sec.next;
+            }
+            sec.next = sec.next.next;
+        }else head = head.next;
+
+        return head;
+    }
+}
