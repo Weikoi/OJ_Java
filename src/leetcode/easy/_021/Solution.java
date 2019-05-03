b193a8c0a031eb6d6acf9a7d038ffe1ddccc521a
huangxiaowei <huangzengrui126@126.com>
2019-05-03 15:26:56 +0800

    add

---
+++ src/leetcode/easy/_021/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.easy._021;
+
+import basic_ds.ListNode;
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+
+        ListNode init = new ListNode(0);
+        ListNode cur = init;
+
+        while(cur1!=null&&cur2!=null){
+            if(cur1.val<cur2.val){
+                cur.next = cur1;
+                cur = cur.next;
+            }else{
+                cur.next = cur2;
+                cur = cur.next;
+            }
+        }
+        if(cur1==null)cur.next=cur2;
+        if(cur2==null)cur.next=cur1;
+        return init.next;
+    }
+}
